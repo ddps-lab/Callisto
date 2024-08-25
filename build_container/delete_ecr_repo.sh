147d253f91b041be2199493472da9f1672785b93
mhsong <mhsong@kookmin.ac.kr>
2024-08-26 05:30:14 +0900

    Fix delete_ecr_repo.sh

--- build_container/delete_ecr_repo.sh
+++ build_container/delete_ecr_repo.sh
@@ -5,3 +5,4 @@ REGION=$2
 AWSCLI_PROFILE=$3
 
 aws ecr delete-repository --force --repository-name callisto-jupyter-base-notebook --region $REGION --profile $AWSCLI_PROFILE
+aws ecr create-repository --repository-name callisto-jupyter-controller --region $REGION --profile $AWSCLI_PROFILE
