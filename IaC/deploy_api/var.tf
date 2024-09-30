7fc35dcc2944a7aa31acacab67c9d0eec595ca90
mhsong <mhsong@kookmin.ac.kr>
2024-09-30 22:06:58 +0900

    Remove jupyter controller api

--- IaC/deploy_api/var.tf
+++
@@ -1,6 +0,0 @@
-variable "region" {}
-variable "route53_domain" {}
-variable "jupyter_controller_function_name" {}
-variable "jupyter_controller_function_arn" {}
-variable "environment" {}
-variable "random_hex" {}
