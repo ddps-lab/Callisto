1886ff0a012ac2e08ff73fa1a13302fe663a3d05
mhsong <mhsong@kookmin.ac.kr>
2024-09-30 23:08:29 +0900

    Remove repository_username/password on Karpenter helm install

--- IaC/kubernetes_cluster/scaling.tf
+++ IaC/kubernetes_cluster/scaling.tf
@@ -17,8 +17,6 @@ resource "helm_release" "karpenter" {
   create_namespace    = true
   name                = "karpenter"
   repository          = "oci://public.ecr.aws/karpenter"
-  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-  repository_password = data.aws_ecrpublic_authorization_token.token.password
   chart               = "karpenter"
   version             = "1.0.1"
   wait                = true
