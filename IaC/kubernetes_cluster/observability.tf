4de41add0edd146debb941e4b5f1b236e8d2f868
Ready-Bridge <hanjungyo5533@gmail.com>
2025-02-23 17:40:09 +0900

    feat: kube-prometheus-stack 배포 및 node-exporter Fargate 제외 설정

--- IaC/kubernetes_cluster/observability.tf
+++ IaC/kubernetes_cluster/observability.tf
@@ -1,55 +1,46 @@
+resource "helm_release" "kube-prometheus-stack" {
+  name = "kube-prometheus-stack"
+  namespace = "monitoring"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart = "kube-prometheus-stack"
+  version = "62.2.1"
+  create_namespace = true
+  timeout = 600
 
-# resource "helm_release" "kube-prometheus-stack" {
-#   name = "kube-prometheus-stack"
-#   namespace = "monitoring"
-#   repository = "https://prometheus-community.github.io/helm-charts"
-#   chart = "kube-prometheus-stack"
-#   version = "62.2.1"
-#   create_namespace = true
-#   set {
-#     name  = "grafana.adminPassword"
-#     # need to change with secure value
-#     value = "password"
-#   }
+  set {
+    name  = "grafana.enabled"
+    value = "false"
+  }
 
-#   set {
-#     name  = "grafana.ingress.enabled"
-#     value = "true"
-#   }
+  set {
+    name  = "prometheus.service.type"
+    value = "LoadBalancer"
+  }
 
-#   set {
-#     name  = "grafana.ingress.ingressClassName"
-#     value = "nginx"
-#   }
+  set {
+    name  = "prometheus.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+    value = "nlb"             
+  }
 
-#   set {
-#     name  = "grafana.ingress.annotations.alb\\.ingress\\.kubernetes\\.io/scheme"
-#     value = "internet-facing"
-#   }
+  set {
+    name  = "prometheus.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
+    value = "internet-facing"
+  }
 
-#   set {
-#     name  = "grafana.ingress.annotations.alb\\.ingress\\.kubernetes\\.io/target-type"
-#     value = "ip"
-#   }
-#   set {
-#     name  = "grafana.ingress.paths[0]"
-#     value = "/monitor"
-#   }
+  set {
+    name  = "prometheus-node-exporter.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].key"
+    value = "eks.amazonaws.com/compute-type"
+  }
 
-#   set {
-#     name  = "prometheus-node-exporter.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].key"
-#     value = "eks.amazonaws.com/compute-type"
-#   }
+  set {
+    name  = "prometheus-node-exporter.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].operator"
+    value = "NotIn"
+  }
 
-#   set {
-#     name  = "prometheus-node-exporter.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].operator"
-#     value = "NotIn"
-#   }
+  set {
+    name  = "prometheus-node-exporter.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].values[0]"
+    value = "fargate"
+  }
 
-#   set {
-#     name  = "prometheus-node-exporter.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].values[0]"
-#     value = "fargate"
-#   }
-
-#   depends_on = [ module.eks, helm_release.aws-load-balancer-controller ]
-# }
+  depends_on = [ module.eks, helm_release.aws-load-balancer-controller ]
+}
