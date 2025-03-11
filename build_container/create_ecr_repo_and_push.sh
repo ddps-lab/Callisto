2cba161d6b36c5a702303cc8e46aa2379ace53ce
mhsong <mhsong@kookmin.ac.kr>
2025-03-11 20:50:05 +0900

    Remove deprecated flag

--- build_container/create_ecr_repo_and_push.sh
+++ build_container/create_ecr_repo_and_push.sh
@@ -12,12 +12,12 @@ aws ecr get-login-password --region $REGION --profile $AWSCLI_PROFILE | docker l
 
 # Jupyter Base Notebook
 cd ./callisto_jupyter_base_notebook
-docker build --platform linux/amd64 -t $ECR_URI/callisto-jupyter-base-notebook --provenance=false .
+docker build --platform linux/amd64 -t $ECR_URI/callisto-jupyter-base-notebook .
 docker push $ECR_URI/callisto-jupyter-base-notebook:latest
 cd -
 
 # Callisto Jupyter Controller
 cd ../IaC/deploy_db_api/lambda_codes/callisto-ddb-jupyter-api
-docker build --platform linux/amd64 -t $ECR_URI/callisto-ddb-jupyter-api --provenance=false .
+docker build --platform linux/amd64 -t $ECR_URI/callisto-ddb-jupyter-api .
 docker push $ECR_URI/callisto-ddb-jupyter-api:latest
 cd -
