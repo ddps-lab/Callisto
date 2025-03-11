9cabd8fce988778cc18f482e6d761634e6f44689
TaeYoon <tykim7@kookmin.ac.kr>
2025-03-11 22:56:44 +0900

    Refactor styled-components to tailwind css

--- frontend/src/pages/forms/styles.jsx
+++ frontend/src/pages/forms/styles.jsx
@@ -1,40 +1,18 @@
-import styled from 'styled-components';
+/* eslint-disable react/prop-types */
 import { Form } from 'antd';
 
-const PageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
-  background: #eee;
-`;
-
-const FormWrapper = styled.div`
-  background: #fff;
-  padding: 10px 20px;
-  border-radius: 8px;
-  box-shadow: 0 0 10px #ccc;
-
-  > .title {
-    font-weight: 100;
-    letter-spacing: -1px;
-  }
-`;
-
 export const FormLayout = ({ children, onFinish }) => (
-  <PageContainer>
-    <FormWrapper>
-      <h1 className="title">Callisto</h1>
+  <div className="flex flex-col w-full h-full justify-center items-center bg-gray-200">
+    <div className="bg-white p-4 md:p-5 rounded-lg shadow-md w-80">
+      <h1 className="font-thin tracking-tight">Callisto</h1>
       <Form
-        style={{ width: '350px' }}
+        className="w-full"
         layout="vertical"
         autoComplete="off"
         onFinish={onFinish}
       >
         {children}
       </Form>
-    </FormWrapper>
-  </PageContainer>
+    </div>
+  </div>
 );
